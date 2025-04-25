b55a0fb2e49af25fd221d666296dd4f2585c29b8
XWilson125 <xavierwilson@student.lccc.wy.edu>
2025-04-25 16:04:33 +0000

    Still working....

--- Basketball_simulation.py
+++ Basketball_simulation.py
@@ -3,7 +3,14 @@ import random
 def RNG(value, variance):
     return value + random.randrange(0-variance, variance)
 
-class default_variables():           #Defines every last variable. Note: all variables are taken from NBA stats and are rough guesstimates for the variation. Classed purely for storage.
+def percent_chance(value):
+    num = random.randrange(0, 100)
+    if num > value:
+        return True
+    else:
+        return False
+
+class default_variables():          #Defines every last variable. Note: all variables are taken from NBA stats and are rough guesstimates for the variation. Classed purely for storage.
     time = 30
     score_needed_to_make = 3
     three_point_percentage = 30
@@ -18,8 +25,51 @@ class default_variables():           #Defines every last variable. Note: all var
     shot_rebound_chance_deviation = 10
     freethrow_rebound_chance_deviation = 7
     win_overtime_chance_deviation = 15
-    def change_variable():
-        print(default_variables)
+    def _input_percentage():                                    #NOT COMPLETE!
+        percent_num = input(input)
+        try:
+            percent_num = float(percent_num)
+        except:
+            raise TypeError("Non-number inserted as a percentage")
+        if 0 <= percent_num <= 100:
+            raise ValueError("Number inserted is not from 0-100")
+        
+    def change_variable():                                      #variable to edit certain stats(like if, for example, the opponent is bad at shooting free throws)
+        print(f'Current variables:\n'                           #Prints stats; necessary to look them over
+              f'    1 - Three point shot chance: {default_variables.three_point_percentage}%\n'
+              f'    2 - Two point shot chance: {default_variables.two_point_percentage}%\n'
+              f'    3 - Opponent free throw chance: {default_variables.freethrow_percentage}%\n'
+              f'    4 - Offensive rebound chance: {default_variables.shot_rebound_chance}%\n'
+              f'    5 - Defensive rebound chance: {default_variables.freethrow_rebound_chance}%\n')
+    #Gathers and validates input
+        varchange = input('Which variable would you like to change? Pick a number or e to exit. ')
+        valid_input = False
+        quit_num = 0
+        while valid_input == False:
+            try:
+                varchange = int(varchange)
+                if 1 <= varchange <= 5:
+                    valid_input = True
+                else:
+                    varchange = input('That is not a valid number. Pick a number from 1 to 5.')
+            except:
+                if quit_num == 3 or varchange == 'e':
+                    print('Assuming that you want to exit the function.')
+                    break
+                print('That is not a valid input. Pick a number.')
+                quit_num += 1
+
+        if varchange == 1:
+            default_variables.three_point_percentage = default_variables._input_percentage()
+        elif varchange == 2:
+            pass
+        elif varchange == 3:
+            pass
+        elif varchange == 4:
+            pass
+        elif varchange == 5:
+            pass
+
     def determine_variables(self):
         self.three_point_percentage = RNG(default_variables.three_point_percentage, default_variables.three_point_percentage_deviation)
         self.two_point_percentage = RNG(default_variables.two_point_percentage, default_variables.two_point_percentage_deviation)
@@ -28,15 +78,36 @@ class default_variables():           #Defines every last variable. Note: all var
         self.freethrow_rebound_chance = RNG(default_variables.freethrow_rebound_chance, default_variables.freethrow_rebound_chance_deviation)
         self.win_overtime_chance = RNG(default_variables.win_overtime_chance, default_variables.win_overtime_chance_deviation)
 
-def three_point_shot():
-    pass
+def three_point_shot(game):
+    print('tpstest')                #REMOVE: USED FOR TESTING!
+    if percent_chance(game.three_point_percentage):
+        return percent_chance(game.win_overtime_chance)
+    else:
+        return False
 '''
 Take a single 3-point shot when time is running out.
 If the shot misses, the game is lost.
 If the shot goes in, overtime is forced, where the outcome is a 50/50 chance.'''
 
-def two_point_foul():
-    pass
+def two_point_foul(game):
+    print('tpftest')                #REMOVE: USED FOR TESTING!
+    not_certain_turnover = True
+    if percent_chance(game.two_point_percentage):
+        game.score_needed_to_make -= 2
+        not_certain_turnover = False
+    while not_certain_turnover and percent_chance(game.shot_rebound_chance):
+        if percent_chance(game.two_point_percentage):
+            game.score_needed_to_make -= 2
+            not_certain_turnover = False
+    
+    while game.time > 0:
+        if percent_chance(game.freethrow_percentage):
+            game.score_needed_to_make += 1
+            certain_rebound = True
+        else:
+            certain_rebound = False
+        if percent_chance(game.freethrow_rebound_chance) or certain_rebound:
+            pass
 '''    Two-Point + Foul Strategy
 
 Attempt a higher-percentage two-point shot.
@@ -56,9 +127,9 @@ two_point_win_count = 0
 for unused in range(sim_run_count):
     game = default_variables()
     game.determine_variables()
-    if three_point_shot() == True: #will use variables to calculate 3-point plan, then add outcome to list
+    if three_point_shot(game) == True: #will use variables to calculate 3-point plan, then add outcome to list
         three_point_win_count += 1
-    if two_point_foul() == True: #will use variables to calculate 2-point-and-foul plan, then add outcome to list
+    if two_point_foul(game) == True: #will use variables to calculate 2-point-and-foul plan, then add outcome to list
         two_point_win_count += 1
 print(f'Three point win percentage: {three_point_win_count/sim_run_count}')
 print(f'Two point and foul win percentage: {two_point_win_count/sim_run_count}')
